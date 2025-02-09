7cf36c32f90ce5214e574b58adb3720b113a7cb8
DFKorn <d1man2192@mail.ru>
2025-02-09 19:38:29 +0300

    create Footer component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/layout/Header";
 import { getGlobalSettings } from "@/data/loaders";
+import { Footer } from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,8 +23,9 @@ export const metadata: Metadata = {
 
 async function loader() {
   const { data } = await getGlobalSettings()
+  //console.dir(data, {depth: null})
   if (!data) throw new Error('Failed to fetch global settings')
-  return { header: data?.header};
+  return { header: data?.header, footer: data?.footer};
 }
 
 export default async function RootLayout({
@@ -31,12 +33,14 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const {header} = await loader()
+  const {header, footer} = await loader()
+  //console.dir(footer, {depth: null})
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header data= {header}/>
         {children}
+        <Footer data={footer}/>
       </body>
     </html>
   );
